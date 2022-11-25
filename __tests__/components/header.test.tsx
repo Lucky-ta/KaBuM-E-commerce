2c794d08d2820deb774dc88617acc20c06748258
Lukita <lucmaieski@gmail.com>
2022-11-25 08:29:44 -0300

    Create header component tests

---
+++ __tests__/components/header.test.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import Header from '../../src/components/Header/Header';
+
+describe('Test main page header component', () => {
+  beforeEach(() => {
+    render(<Header />);
+  });
+
+  it('should have an image', () => {
+    const headerImage = screen.getByRole('img');
+    expect(headerImage).toBeInTheDocument();
+  });
+
+  it('should have an search input', () => {
+    const headerSearchInput = screen.getByAltText(/Busca.../i);
+    expect(headerSearchInput).toBeInTheDocument();
+  });
+
+  it('should have an search button', () => {
+    const headerButtons = screen.getAllByRole('button');
+    expect(headerButtons[0]).toBeInTheDocument();
+  });
+
+  it('should have an cart button', () => {
+    const headerButtons = screen.getAllByRole('button');
+    expect(headerButtons[1]).toBeInTheDocument();
+  });
+
+  it('should have an more options button', () => {
+    const headerButtons = screen.getAllByRole('button');
+    expect(headerButtons[2]).toBeInTheDocument();
+  });
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import Form from '../components/Form';
+import Header from '../components/Header/Header';
 
 export default function Home() {
   return (
-    <Form />
+    <Header />
   );
 }

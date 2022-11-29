71af3757e11ae47a44cd730ff10cebf969fc6ea1
Lukita <lucmaieski@gmail.com>
2022-11-29 15:58:12 -0300

    Remove styled components

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,26 +3,25 @@ import React from 'react';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { FaShoppingCart } from 'react-icons/fa';
 import { IoReorderThreeOutline } from 'react-icons/io5';
-import * as HeaderComponents from './index';
 import KabumLogo from '../../public/favicon/kabum.png';
 
 function Header() {
   return (
-    <HeaderComponents.HeaderContainer>
-      <HeaderComponents.KabumLogo src={KabumLogo.src} alt="kabum-logo" />
+    <div>
+      <img src={KabumLogo.src} alt="kabum-logo" />
       <div>
-        <HeaderComponents.SearchInput type="text" aria-label="Busca..." placeholder="Busca..." />
-        <HeaderComponents.SearchButton type="button">
+        <input type="text" aria-label="Busca..." placeholder="Busca..." />
+        <button type="button">
           <AiOutlineSearch />
-        </HeaderComponents.SearchButton>
+        </button>
       </div>
-      <HeaderComponents.TransparentButton type="button">
+      <button type="button">
         <FaShoppingCart />
-      </HeaderComponents.TransparentButton>
-      <HeaderComponents.TransparentButton type="button">
+      </button>
+      <button type="button">
         <IoReorderThreeOutline />
-      </HeaderComponents.TransparentButton>
-    </HeaderComponents.HeaderContainer>
+      </button>
+    </div>
   );
 }
 

d250f0415b00f3ecfc8cd80735ed59ed2e56c01b
Lukita <lucmaieski@gmail.com>
2022-11-29 17:27:19 -0300

    Set css to header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,20 +7,30 @@ import KabumLogo from '../../public/favicon/kabum.png';
 
 function Header() {
   return (
-    <div>
-      <img src={KabumLogo.src} alt="kabum-logo" />
-      <div>
-        <input type="text" aria-label="Busca..." placeholder="Busca..." />
-        <button type="button">
+    <div className="flex items-center p-3 bg-[#0060b1]">
+      <img width="45em" src={KabumLogo.src} alt="kabum-logo" />
+      <div className="w-full">
+        <input
+          className="rounded-md px-2.5 py-1.5 ml-4 w-4/5"
+          type="text"
+          aria-label="Busca..."
+          placeholder="Busca..."
+        />
+        <button
+          className="absolute text-slate-500 -translate-x-6 pt-2.5 pb-3 pr-2"
+          type="button"
+        >
           <AiOutlineSearch />
         </button>
       </div>
-      <button type="button">
-        <FaShoppingCart />
-      </button>
-      <button type="button">
-        <IoReorderThreeOutline />
-      </button>
+      <div className="flex gap-4 m-auto mr-2">
+        <button className="text-xl text-stone-300/50" type="button">
+          <FaShoppingCart />
+        </button>
+        <button className="text-3xl text-stone-300/50" type="button">
+          <IoReorderThreeOutline />
+        </button>
+      </div>
     </div>
   );
 }
